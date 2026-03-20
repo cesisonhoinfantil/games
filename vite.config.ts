b3a2e5f732581640bdd374755a0dbddd7429f2ac
Pedro Ryan <prgames1234@gmail.com>
2026-03-19 21:15:40 -0300

    feat: transform app in a pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,53 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'new-logo.png', 'meta-image.png'],
+      manifest: {
+        name: 'CESI Jogos',
+        short_name: 'CESI',
+        description: 'Jogos Educativos e Interativos da escola CESI',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,mp3,wav,json,lottie,woff,woff2,ttf}'],
+        maximumFileSizeToCacheInBytes: 15 * 1024 * 1024
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
